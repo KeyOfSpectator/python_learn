3a60714503a9a5066f01810dd8a5d263a82edeff
KeyOfSpectator <413070456@qq.com>
2014-10-07 14:04:37 +0800

    add list.py

---
+++ list.py
@@ -0,0 +1,45 @@
+#list 
+
+classmates = ['zhichen','wuhao','fengshichun']
+
+print classmates
+
+print len(classmates)
+
+print classmates[1]
+
+print classmates[0]
+
+#print classmates[3]
+
+print classmates[-1]
+
+classmates.append('Amanda')
+
+print classmates
+
+classmates.insert(2,'wangchenmin')
+
+print classmates
+
+classmates.pop()
+
+print classmates
+
+classmates.pop(3)
+
+print classmates
+
+classmates[0] = 'fengshichun'
+print classmates
+
+classmates.append(123)
+print classmates
+
+print "=============="
+list2 = [123,'test',['sub1','sub2'],True]
+print list2
+print len(list2)
+
+print list2[2][1]
+
