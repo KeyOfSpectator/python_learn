2b4a2161802307703ab616e5b6a155774fbd7f1f
KeyOfSpectator <413070456@qq.com>
2014-10-07 14:04:37 +0800

    add advance1.py

---
+++ advance1.py
@@ -0,0 +1,27 @@
+#advance1.py
+
+'''
+L = []
+n = 100
+while n > 0:
+	L.append(n)
+	n = n-1
+print L
+'''
+
+"""
+asdf
+"""
+
+L = [1,2,3,4,5,6,7,8,9,10]
+
+print L[2:4]
+print L[:4]
+
+print L[-2:]
+
+print L[::5]
+
+print L[2::5]
+
+print L[:]
